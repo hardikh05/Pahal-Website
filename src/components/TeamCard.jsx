1d22271f6e7e98b1cc11f31b35ec34b83b6d2e7d
nikhilmishra1710 <nikhilmishra1710@gmail.com>
2024-08-22 02:16:38 +0530

    chore: Update TeamCard component with hover animation

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./TeamCard.css";
-import { FaSquareInstagram, FaLinkedin } from "react-icons/fa6";
+import { FaSquareInstagram, FaLinkedin, FaInstagram } from "react-icons/fa6";
 
 const TeamCard = (props) => {
   let patcoord = props.patcoord;
   let founders = props.founders;
   let web = props.web;
+
+  const [isHovered, setIsHovered] = React.useState(false);
   if (patcoord) {
     return (
       <div className="card mb-10">
@@ -39,11 +41,11 @@ const TeamCard = (props) => {
           <div className="text-opacity-60 mt-1">{web.desg2}</div>
         </div>
         <div className="links flex justify-center items-center space-x-4 mt-3">
-          <a href={web.iglink} className="icon">
-            <FaSquareInstagram />
+          <a href={web.iglink} className="icon cursor-pointer bg-black p-1 hover:bg-[linear-gradient(120deg,#f9ce34,#ee2a7b,#6338d7)] transition-all hover:rounded-full anim">
+            <FaInstagram fill="#fff" />
           </a>
-          <a href={web.linkedinlink} className="icon">
-            <FaLinkedin />
+          <a href={web.linkedinlink} className="icon cursor-pointer bg-black p-1 hover:bg-[#002aff] hover:rounded-full anim">
+            <FaLinkedin fill="#fff" />
           </a>
         </div>
       </div>
